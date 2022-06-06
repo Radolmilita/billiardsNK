5fdf9d40ed4e9b9d05ae06c3b86323e8ad675817
Dimitriev Valerij <65241148+Radolmilita@users.noreply.github.com>
2022-06-06 03:50:52 +0300

    Add files via upload

--- PaymentViewController.swift
+++ PaymentViewController.swift
@@ -2,10 +2,11 @@
 //  PaymentViewController.swift
 //  nk
 //
-//  Created by Валерий on 05.06.2022.
+//  Created by Валерий on 15.03.2021.
 //
 
 import UIKit
+import PassKit
 
 class PaymentViewController: UIViewController {
 
@@ -14,9 +15,23 @@ class PaymentViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-    
-    
-    
+    private var paymentRequest: PKPaymentRequest = {
+            let request = PKPaymentRequest()
+            request.merchantIdentifier = "merchant.com..."
+            request.supportedNetworks = [.visa, .masterCard,.amex,.discover]
+            request.supportedCountries = ["UA"]
+            request.merchantCapabilities = .capability3DS
+            request.countryCode = "UA"
+            request.currencyCode = "UAH"
+            request.paymentSummaryItems = [PKPaymentSummaryItem(label: "App test", amount: 10.99)]
+            return request
+        }()
+    @IBAction func purchase(_ sender: Any?) {
+            if let controller = PKPaymentAuthorizationViewController(paymentRequest: paymentRequest) {
+                controller.delegate = self
+                present(controller, animated: true, completion: nil)
+            }
+        }
 
     /*
     // MARK: - Navigation
@@ -28,4 +43,16 @@ class PaymentViewController: UIViewController {
     }
     */
 
+}
+
+extension ViewController: PKPaymentAuthorizationViewControllerDelegate {
+ 
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
+    }
+ 
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+ 
 }
